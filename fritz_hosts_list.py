146dcb381215895d0db3ca571bd418de11525111
Tony Davis <tony@virtuallytd.com>
2023-12-07 15:06:16 +0100

    initial code upload

---
+++ fritz_hosts_list.py
@@ -0,0 +1,48 @@
+import argparse
+from fritzconnection.lib.fritzhosts import FritzHosts
+from tabulate import tabulate
+import sys
+
+# Set your Fritz!Box credentials here.
+FRITZ_IP = '192.168.1.1'  # Replace with your Fritz!Box IP address
+FRITZ_USER = 'admin'      # Replace with your Fritz!Box username
+FRITZ_PASSWORD = 'password'  # Replace with your Fritz!Box password
+
+def get_hosts_info():
+    # Establishes a connection to the Fritz!Box.
+    print("Connecting to Fritz!Box...")
+    fritz_hosts = FritzHosts(address=FRITZ_IP, user=FRITZ_USER, password=FRITZ_PASSWORD)
+
+    # Retrieve information about connected hosts.
+    print("Gathering host data...")
+    hosts_info = []
+    for index in range(fritz_hosts.host_numbers):
+        host = fritz_hosts.get_generic_host_entry(index)
+        hosts_info.append([host['NewHostName'], host['NewIPAddress'], host['NewMACAddress']])
+        # Print the progress on the same line.
+        sys.stdout.write(f"\rRetrieved data for host {index + 1} of {fritz_hosts.host_numbers}")
+        sys.stdout.flush()
+    print()  # Move to the next line after the loop
+    return hosts_info
+
+# Set up argument parsing for command line options.
+parser = argparse.ArgumentParser(description="List and sort Fritz!Box hosts.")
+parser.add_argument("-s", "--sort", choices=['ip', 'hostname', 'mac'], help="Sort by: 'ip', 'hostname', or 'mac'")
+parser.add_argument("-o", "--output", help="Output to a file (e.g., -o ./out.txt)")
+args = parser.parse_args()
+
+# Retrieve and optionally sort hosts information.
+hosts_info = get_hosts_info()
+print("\nSorting and displaying data...")
+# Add sorting functionality here if needed
+
+# Handle output: either print to console or write to a file.
+if args.output:
+    with open(args.output, 'w') as file:
+        file.write(tabulate(hosts_info, headers=['Hostname', 'IP', 'MAC']))
+    print(f"Data written to {args.output}")
+else:
+    print(tabulate(hosts_info, headers=['Hostname', 'IP', 'MAC']))
+
+print("Operation completed.")
+
